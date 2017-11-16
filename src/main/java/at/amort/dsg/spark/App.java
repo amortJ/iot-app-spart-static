2bcf1cfef2313decfd1dc016d68b6996b4a660cf
Jan <24584216+amortJ@users.noreply.github.com>
2017-11-16 20:20:44 +0100

    to java 1.7

--- src/main/java/at/amort/dsg/spark/App.java
+++ src/main/java/at/amort/dsg/spark/App.java
@@ -1,5 +1,9 @@
 package at.amort.dsg.spark;
 
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
 import static spark.Spark.*;
 
 /**
@@ -12,6 +16,20 @@ public class App
     {
         System.out.println( "IoT measurement" );
 
-        get("/average", (req, res) -> { res.type("application/json"); return "{ \"average\": 42.1 }";});
+        //get("/average", (req, res) -> { res.type("application/json"); return "{ \"average\": 42.1 }";});
+
+        Route route = new Route() {
+            @Override
+            public Object handle(Request request, Response response) throws Exception {
+
+                System.out.println("foo");
+                response.type("application/json");
+                response.body("{ \"average\": 42.1 }");
+
+                return "{ \"average\": 42.1 }";
+            }
+        };
+
+        get("/average", route);
     }
 }
